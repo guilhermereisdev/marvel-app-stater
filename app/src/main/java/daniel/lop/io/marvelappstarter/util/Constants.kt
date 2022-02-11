c5404abcd2014e326b7f8f27ae11e9bcdeb1cda9
Guilherme Reis <guilhermereis2009@hotmail.com>
2022-02-11 03:21:10 -0300

    Configurada a injeção de dependências e ServiceApi.

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,14 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+    const val PUBLIC_KEY = "591bb6eae2fc68d8da503442c18badc3"
+    const val PRIVATE_KEY = "dd4888d313aee54bb90cf7267e33c5d5b9190e9d"
+
+    const val API_KEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+
+}
