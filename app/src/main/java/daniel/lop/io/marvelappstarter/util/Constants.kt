169cbcfdb3f7ec21832274d52e48fad96d4e3197
Guilherme Reis <guilhermereis2009@hotmail.com>
2022-02-16 02:45:57 -0300

    Implementado o SearchCharacterFragment.kt

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -7,6 +7,9 @@ object Constants {
     const val PUBLIC_KEY = "591bb6eae2fc68d8da503442c18badc3"
     const val PRIVATE_KEY = "dd4888d313aee54bb90cf7267e33c5d5b9190e9d"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
     const val API_KEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
