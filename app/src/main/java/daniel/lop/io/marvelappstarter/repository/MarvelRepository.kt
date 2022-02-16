b35cdc5d5abf1320d9510e71353e923e24fa402c
Guilherme Reis <guilhermereis2009@hotmail.com>
2022-02-16 00:42:46 -0300

    Implementado o ListCharacterViewModel.kt

--- app/src/main/java/daniel/lop/io/marvelappstarter/repository/MarvelRepository.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/repository/MarvelRepository.kt
@@ -6,6 +6,6 @@ import javax.inject.Inject
 class MarvelRepository @Inject constructor(
     private val api: ServiceApi
 ) {
-    suspend fun list(nameStartsWith: String?) = api.list(nameStartsWith)
+    suspend fun list(nameStartsWith: String? = null) = api.list(nameStartsWith)
     suspend fun getComics(characterId: Int) = api.getComics(characterId)
 }
